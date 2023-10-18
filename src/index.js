cfc51181f7d0e83e6a3b72523a2563ac6c3fd7e8
Bruna <brn.flor@gmail.com>
2023-10-18 21:06:35 +0100

    getting searched pokemon from api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const searchedWord = document.getElementById("searchInput")
 const buttonSearch = document.getElementById("buttonSearch")
 
-const searchPokemon = () => {
-    console.log(searchedWord.value)
+const searchPokemon = async() => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchedWord.value}`)
+    const data = await response.json()
+    console.log(data)
 }
 
 buttonSearch.addEventListener('click', searchPokemon)
