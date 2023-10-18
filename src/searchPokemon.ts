7312725535bd6ed70b91e6c39a6cb50846d84c08
Bruna <brn.flor@gmail.com>
2023-10-18 22:42:44 +0100

    testing data structure to type

--- src/searchPokemon.ts
+++ src/searchPokemon.ts
@@ -1,15 +1,16 @@
 export const searchedPokemon = async(element: HTMLButtonElement) => {
     const input = document.getElementById("searchInput") as HTMLInputElement
     const buttonSearch = document.getElementById("buttonSearch")
+    
     const searchApi = async() => {
         const searchBy = input.value
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchBy}`)
         const data = await response.json()
         console.log(data)
-        element.innerHTML = `pokemon is ${data.name}`
+        element.innerHTML = `pokemon is type ${data.types[0].type.name}`
 
         return data
     }
-    buttonSearch?.addEventListener('click', searchApi)
 
+    buttonSearch?.addEventListener('click', searchApi)
 }
